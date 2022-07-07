f0fb765271c1cbb97da2af5e53c1b43d9b3fc23b
Muringo <muringolucie@gmail.com>
2022-07-07 12:04:37 +0300

    A simple travis excercise

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const FAV_COURSE = process.env.NAME;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log('${FAV_COURSE} rocks!');
     await sleep(5000);
   }
 }
